7eb86662e15950cb74c0d2b13db6948c8acf2018
Bridge Tools <robot@bridgetools.io>
2021-12-03 13:53:04 +0200

    Card component

--- design-system/src/index.js
+++ design-system/src/index.js
@@ -10,6 +10,7 @@ export { colors, typography, spacing } from "./tokens";
 
 export { default as Avatar } from "./components/Avatar";
 export { default as Button } from "./components/Button";
+export { default as Card } from "./components/Card";
 export { default as Carousel } from "./components/Carousel";
 export { default as Icon } from "./components/Icon";
 export { default as Select } from "./components/Select";
